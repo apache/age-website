a3acccb45a79c39f95725961b10d6c659cfdfb81
Hanbyeol Shin /  David Shin / 신한별 <76985229+shinhanbyeol@users.noreply.github.com>
2022-08-23 13:49:24 +0900

    Update config & script
    1. deploy script update
    2. path prefix change

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description:
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
   },
-  pathPrefix: "/age-community",
+  pathPrefix: "/",
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
