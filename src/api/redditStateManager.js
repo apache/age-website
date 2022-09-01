1180267d47edd0dc41ea2b2d753379e82dd6a364
Oh Shin Yeop <46528849+ohshinyeop@users.noreply.github.com>
2022-09-01 16:58:29 +0900

    edit external link (#78)
    * RQ-1004
    
    * community-join / landing page
    
    * landing page style
    
    * landing page / footer / add perfect scrollbar module
    
    * 220823
    
    * edit projectDetai.js
    
    * 220826
    
    * edit reddit spinner
    
    * edit navbar link
    
    * edit landing page
    
    * modified landing page
    
    * modified landing page
    
    * modified landing page
    
    * color theme change
    
    * modifed external link
    
    * Update index.md
    
    Co-authored-by: Hanbyeol Shin /  David Shin / 신한별 <76985229+shinhanbyeol@users.noreply.github.com>

--- src/api/redditStateManager.js
+++ src/api/redditStateManager.js
@@ -7,8 +7,8 @@ async function getRedditDataSetter(stateSetter) {
     .get('https://apacheage.shop:3999/api/v1/reddit/rss')
     .then((res) => {
       let item = res.data.list;
-      for(let i = 0 ; i < item.length; i++) {        
-        item[i].content[0]['_'] = item[i].content[0]['_'].replaceAll("<a" , "<a target='blank'")
+      for(let i = 0 ; i < item.length; i++) {
+        item[i].content[0]['_'] = item[i].content[0]['_'].replaceAll("<a" , "<a target='_blank'")
         item[i].content[0]['_'] = item[i].content[0]['_'].replaceAll('href="/r/' , 'href="https://www.reddit.com/r/')
       }
       stateSetter(item);
