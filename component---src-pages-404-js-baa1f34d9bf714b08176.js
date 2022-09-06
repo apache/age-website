a85de4892924f5342a5677dc4fe7b718999a4309
shinhanbyeol <shinhanbyeol@users.noreply.github.com>
2022-09-06 05:00:08 +0000

    deploy: 471dfad03b2fab1e2e33d9eed25d56f8744135ba

---
+++ component---src-pages-404-js-baa1f34d9bf714b08176.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapahce_age_project=self.webpackChunkapahce_age_project||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),l=a(2891);t.default=function(){return n.createElement(l.Z,null,n.createElement("div",null,n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-baa1f34d9bf714b08176.js.map
---
+++ component---src-pages-contact-thanks-js-530a8215b78df796d50f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapahce_age_project=self.webpackChunkapahce_age_project||[]).push([[561],{6349:function(e,a,t){t.r(a);var n=t(7294),c=t(2891);a.default=function(){return n.createElement(c.Z,null,n.createElement("section",{className:"section"},n.createElement("div",{className:"container"},n.createElement("div",{className:"content"},n.createElement("h1",null,"Thank you!"),n.createElement("p",null,"This is a custom thank you page for form submissions")))))}}}]);
+//# sourceMappingURL=component---src-pages-contact-thanks-js-530a8215b78df796d50f.js.map
