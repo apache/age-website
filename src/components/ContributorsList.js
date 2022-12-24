fa44bd7d0b26acb36698059a4093364714aa48d8
moon19960501@gmail.com <wenzhenhe1@gmail.com>
2022-12-23 16:35:28 -0800

    medal update

--- src/components/ContributorsList.js
+++ src/components/ContributorsList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import * as styles from './styles/AgeContributors.module.scss';
+import medalImage from '../../static/img/medal.jpg'
 
 const ContributorsList = ({ index, user, avatar, html, last }) => {
     return (
         <div className={styles.Users} style={last ? {paddingBottom: "0"} : {paddingBottom: "1.5rem"}}>
             <div className={styles.Rank}>
-                <p style={last ? {opacity: "0"} : {opacity: "1"}}>{index+1}</p>
+                {last ? <img src={medalImage}></img> : <p>{index+1}</p>}
                 <a href={html} style={avatar ? {visibility: "visible"} : {visibility: "hidden"}}><img className={styles.Avatar} src={avatar}></img></a>
             </div>
             <div className={styles.User}>
