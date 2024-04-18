6067e31d2e7512db1b45faae22c563c60bd7fcfe
dpdjvhxm <jhl8710@naver.com>
2024-04-18 17:24:36 +0900

    Update landing page article

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -69,6 +69,12 @@ export const IndexPageTemplate = ({
             <div className={styles.cardRight}>
               Key Updates
               <div className={styles.cardContent}>
+                <div><b>ASF PROJECT SPOTLIGHT: APACHE AGE </b></div>
+                  <div>Read the full blog post now and be a part of shaping the future of graph database technology. </div>
+                    <div>
+                    <a href="https://news.apache.org/foundation/entry/asf-project-spotlight-apache-age" target="_blank" rel="noopener noreferrer">Read More</a></div>
+                  <br></br>
+
                 <div><b>Why do you use Apache AGE? </b></div>
                     <div>Give feedback at the GitHub (<a href="https://github.com/apache/age/issues/1705" target="_blank" rel="noopener noreferrer">#1705</a>)</div>
                     <br></br>
@@ -130,8 +136,31 @@ export const IndexPageTemplate = ({
           <section>
               <div className={styles.card1}>
                 <div className={styles.content}>
-                  <h2><b>This week's article by community</b></h2><hr></hr>
-                    <iframe src="./img/blog2.html" style={{ border: 'none' }} width="100%" height="600" title="Embedded Page"></iframe>
+                  <h3><b>This week's article by community</b></h3><hr></hr>
+
+                  <h2>Graph Database in PostgreSQL: Apache AGE</h2>
+
+              <p>
+                Graph database have gained popularity in recent years due to their ability to handle complex relationships between data. Unlike traditional relational database, which store data in tables, graph database represent data as nodes, edges, and properties. Nodes represent entities, edges represent the relationships between those entities, and properties represent the attributes of both.
+              </p>
+
+              <p>
+                PostgreSQL, a popular relational database, can also function as a graph database through the use of an extension called Apache AGE. With Apache AGE, users can leverage the flexibility and scalability of graph database while still utilizing PostgreSQL's advanced SQL querying capabilities and transaction support.
+              </p>
+
+              <p>
+                To use Apache AGE, users must first install it as an extension and then model their data as nodes and edges. Apache AGE comes with its own set of SQL extensions, similar to Cypher, that allows users to query their graph database. However, users can also still use SQL to query their graph database if desired.
+              </p>
+
+              <p>
+                For those new to graph database, Apache AGE comes with a tutorial to help them get started. Additionally, you may use the #apache-age tag for questions on Stack Overflow, join the project's Discord channel, or open an issue on GitHub, the Apache AGE community is readily available to provide support and answer questions.
+              </p>
+
+              <p>
+                Overall, graph database offer a new way of thinking about how to store and query complex relationships between data. With Apache AGE, users can easily transform their PostgreSQL database into a graph database and take advantage of the benefits that come with this type of database.
+              </p>
+
+
                   </div>
               </div>
             </section>
@@ -142,7 +171,7 @@ export const IndexPageTemplate = ({
           <section>
               <div className={styles.card1}>
                 <div className={styles.content}>
-                  <h2><b>Last week's article by community</b></h2><hr></hr>
+                  <h3><b>Last week's article by community</b></h3><hr></hr>
                     <iframe src="./img/blog1.html" style={{ border: 'none' }} width="100%" height="600" title="Embedded Page"></iframe>
                   </div>
               </div>
