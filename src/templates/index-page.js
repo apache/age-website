5d5f275a37bf8f30c1be9b9af0eefa09e0de8d2a
dpdjvhxm <jhl8710@naver.com>
2024-06-12 16:03:59 +0900

    modified:   src/templates/index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -62,16 +62,13 @@ export const IndexPageTemplate = ({
                 Through Apache AGE, PostgreSQL users will gain access to graph query modeling within the existing relational database.
                 <br></br><br></br>
                 Users can read and write graph data in nodes and edges. They can also use various algorithms such as variable length and edge traversal when analyzing data.
-
               </div>
             </div>
-            <div className={styles.verticalLine}>
-            </div>
+            <div className={styles.verticalLine}></div>
             <div className={styles.cardRight}>
               <div className={styles.cardContent}>
                   <img src="/img/AGE-Architecture.webp" alt="img"></img>
                 <br></br>
-
               </div>
             </div>
           </div>
@@ -80,24 +77,6 @@ export const IndexPageTemplate = ({
           <div><br></br></div>
           <div><br></br></div>
           <div><br></br></div>
-
-
-          {/* 메인화면: 유튜브 영상
-          <section className={styles.videos}>
-            <div className={styles.content}>
-              <h2>Installation Guide</h2>
-              <div className={styles.Youtube}>
-                <iframe
-                  src="https://www.youtube.com/embed/0-qMwpDh0CA"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            </div>
-          </section>
-          */ }
           
           <section>
               <div className={styles.card1}>
@@ -141,8 +120,8 @@ export const IndexPageTemplate = ({
               <div className={styles.card1}>
                 <div className={styles.content}>
                   <h3><b>This week's article by community</b></h3><hr></hr>
-
                   <h2>Graph Database in PostgreSQL: Apache AGE</h2>
+                  <div className={styles.cardContent}>
                     <p>
                       Graph database have gained popularity in recent years due to their ability to handle complex relationships between data. Unlike traditional relational database, which store data in tables, graph database represent data as nodes, edges, and properties. Nodes represent entities, edges represent the relationships between those entities, and properties represent the attributes of both.
                     </p>
@@ -159,108 +138,16 @@ export const IndexPageTemplate = ({
                       Overall, graph database offer a new way of thinking about how to store and query complex relationships between data. With Apache AGE, users can easily transform their PostgreSQL database into a graph database and take advantage of the benefits that come with this type of database.
                     </p>
                   </div>
-              </div>
-          </section>
-
-          <div><br></br></div>
-          <div><br></br></div>
-
-          <section>
-              <div className={styles.card1}>
-                <div className={styles.content}>
-                  <h3><b>Last week's article by community</b></h3><hr></hr>
-                    <iframe src="./img/blog1.html" style={{ border: 'none' }} width="100%" height="600" title="Embedded Page"></iframe>
                   </div>
               </div>
           </section>
 
           <div><br></br></div>
           <div><br></br></div>
-          <div><br></br></div>
-          <div><br></br></div>
 
-          <section>
-            <div className={styles.card1}>
-
-              <div className={styles.content}>
-                <h2>Comparison of Apache AGE, PostGraphile, and Hasura</h2><br></br><br></br>
-                <p>
-                <h3><b>◾ Apache AGE: Graph Database for PostgreSQL</b></h3>
-                Apache AGE extends PostgreSQL, transforming it into a graph database.
-                This enables handling complex data relationships and graph structures, supplemented by the power of Cypher queries for graph-specific operations.<br></br><br></br>
-                
-                <ul><b>Key Features:</b></ul>
-                  <li>Graph & SQL Queries</li>
-                  <li>Integration deeply with existing PostgreSQL features,
-                  making it a robust and scalable solution for graph data management without the need for external graph databases.</li>
-                  <li>Ideal for applications that require complex data relationship analysis,such as social networks, recommendation systems, and knowledge graphs.</li>
-                  <br></br><hr></hr><br></br>
-
-                <h3><b>◾ PostGraphile: GraphQL API from PostgreSQL Schema</b></h3>
-                PostGraphile is a tool for building a GraphQL API on top of an existing PostgreSQL schema.<br></br>
-                It's designed for efficiency in web development, allowing real-time data access and manipulation through GraphQL interface.<br></br><br></br>
-
-                <ul><b>Key Features:</b></ul>
-                  <li>Automates the creation of GraphQL APIs from existing PostgreSQL.</li>
-                  <li>Offers subscriptions for real-time data updates.</li>
-                  <li>Best suited for projects requiring quick development of web or mobile backends with real-time data needs, but without complex graph data management requirements.</li><br></br><hr></hr><br></br>
-
-                <h3><b>◾ Hasura: Real-Time GraphQL Engine for PostgreSQL</b></h3>
-                Hasura is a GraphQL server engine that dynamically generates real-time GraphQL APIs from a PostgreSQL database.<br></br>
-                It emphasizes performance and developer productivity, with an easy setup process and tools for managing access and queries.<br></br><br></br>
-
-                <ul><b>Key Features:</b></ul>
-                  <li>Provides a user-friendly interface for setting up and managing GraphQL endpoints.</li>
-                  <li>Optimized for performance, supporting high-volume traffic.</li>
-                  <li>Ideal for developers needing a fast, scalable GraphQL layer over their PostgreSQL databases.</li>
-                  <br></br><hr></hr><br></br>
-
-                <h3><b>◾ Conclusion</b></h3>
-                Apache AGE is ideal for sophisticated graph data tasks within PostgreSQL.<br></br>
-                PostGraphile and Hasura excel in quick GraphQL API deployment for example for web and mobile, complementing AGE's rich graph features.<br></br>
-                </p>
-
-                </div>
-              </div>
-            </section>
-
-          <div><br></br></div>
-          <div><br></br></div>
-          <div><br></br></div>
-          <div><br></br></div>
-
-      </div>
-
-      </section>
-      {/* <section style={{ backgroundPosition: 'center', backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }} hidden > */}
-        {/* <div className={styles.content}>
-          <h2>Message From AGE Team</h2>
-          <div className={styles.Youtube}>
-            <iframe
-              src="https://www.youtube.com/embed/0-qMwpDh0CA"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div> */}
-      {/* </section> */}
-
-
-      {/* 메인화면: 디스코드
-      <section style={{ backgroundPosition: 'center', backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>
-        <div className={styles.content}>
-          <h2>Join AGE Discord</h2>
-            <a href="https://discord.com/invite/NMsBs9X8Ss" target="_blank">
-              <div className={styles.inviteDiscord}>
-                <img src={ageLogo} alt="Discord Invite Link" />
-              </div>          
-            </a>
-          <h3>Got any questions about the project? Join us in Discord and let's chat!</h3>
         </div>
       </section>
-      */}
+
       <section hidden></section>
       
       <section>
